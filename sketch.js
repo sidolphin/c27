981b1cc19e4346ad29c5c60fafcf887a19d76e5e
sidolphin <sidwadhwa17@gmail.com>
2020-04-22 20:05:14 +0530

    c27

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var logobj,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    logobj=new Log(230,180,80,PI/2);
+    chain= new Chain(bird.body,logobj.body);
 
 }
 
@@ -60,5 +64,8 @@ function draw(){
     log5.display();
 
     bird.display();
+
     platform.display();
+    logobj.display();
+    chain.display();
 }
